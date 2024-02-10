3c29e0e22ce821f5b844a229860ec6f10f29bcb3
s.zuev <s.zuev@LYT-NB-024>
2024-02-10 21:30:26 +0300

    update dependencies

--- MetaGap/MetagapUserCode/settings.py
+++ MetaGap/MetagapUserCode/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.admindocs",
-    "bootstrap5",
+    'bootstrap5',
 ]
 
 # Middleware framework

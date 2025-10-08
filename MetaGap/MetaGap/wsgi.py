b485f782d63faff0d7230a2a9d0442ec40c76228
Stepanvar <84638149+Stepanvar@users.noreply.github.com>
2025-10-08 01:44:45 -0700

    Consolidate Django settings module

--- MetaGap/MetaGap/wsgi.py
+++ MetaGap/MetaGap/wsgi.py
@@ -19,9 +19,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault(
-	'DJANGO_SETTINGS_MODULE',
-	'MetaGap.settings')
+SETTINGS_MODULE = "MetaGap.settings"
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION

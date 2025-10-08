5c8b84cf77ce4648e1d172df276a4eab01695524
Stepanvar <84638149+Stepanvar@users.noreply.github.com>
2025-10-08 04:20:37 -0700

    Fix sample group search and table view

--- MetaGap/app/migrations/0012_merge_20250304_0001.py
+++ MetaGap/app/migrations/0012_merge_20250304_0001.py
@@ -4,7 +4,6 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("app", "0011_alter_samplegroup_created_by"),
         ("app", "0011_inputquality_remove_samplegroup_allele_frequency_and_more"),
     ]
 

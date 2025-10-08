ff76dd1df3d6b864691053056c7d1ee189e85e26
Stepanvar <84638149+Stepanvar@users.noreply.github.com>
2025-10-08 01:30:50 -0700

    Improve VCF import and metadata models

--- MetaGap/app/signals.py
+++ MetaGap/app/signals.py
@@ -1,11 +1,12 @@
+from django.apps import apps
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django.contrib.auth.models import User
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        OrganizationProfile = apps.get_model('app', 'OrganizationProfile')
+        OrganizationProfile = apps.get_model("app", "OrganizationProfile")
         OrganizationProfile.objects.create(user=instance)
 
 @receiver(post_save, sender=User)

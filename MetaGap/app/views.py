9240892a525c592d7b7868d0e360814da15cb29b
Stepanvar <84638149+Stepanvar@users.noreply.github.com>
2025-10-09 14:13:39 +0300

    Adjust variant table priority columns

--- MetaGap/app/views.py
+++ MetaGap/app/views.py
@@ -533,13 +533,16 @@ class SampleGroupDetailView(LoginRequiredMixin, DetailView):
         priority_columns = [
             "chrom",
             "pos",
-            "variant_id",
             "ref",
             "alt",
             "qual",
             "filter",
             "info__af",
+            "info__ac",
+            "info__an",
             "info__dp",
+            "info__mq",
+            "variant_id",
             "format__genotype",
         ]
         table_class = create_dynamic_table(

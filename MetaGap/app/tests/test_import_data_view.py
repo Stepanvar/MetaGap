49474473610d97d1482fc37f182435ad0557de9d
Stepanvar <84638149+Stepanvar@users.noreply.github.com>
2025-10-09 16:11:31 +0300

    Preserve INFO fields as strings

--- MetaGap/app/tests/test_import_data_view.py
+++ MetaGap/app/tests/test_import_data_view.py
@@ -108,7 +108,7 @@ class ImportDataViewIntegrationTests(TestCase):
         self.assertEqual(allele.chrom, "1")
         self.assertEqual(allele.pos, 1234)
         self.assertEqual(allele.variant_id, "rsTest")
-        self.assertAlmostEqual(allele.info.af, 0.5)
+        self.assertAlmostEqual(float(allele.info.af), 0.5)
         self.assertEqual(allele.info.additional["clnsig"], "Pathogenic")
         self.assertEqual(allele.format.genotype, "0/1")
         self.assertEqual(allele.format.fields["gq"], "99")
@@ -299,7 +299,7 @@ class ImportDataViewIntegrationTests(TestCase):
         self.assertEqual(allele.chrom, "1")
         self.assertEqual(allele.pos, 1234)
         self.assertEqual(allele.variant_id, "rsTest")
-        self.assertAlmostEqual(allele.info.af, 0.5)
+        self.assertAlmostEqual(float(allele.info.af), 0.5)
         self.assertEqual(allele.format.genotype, "0/1")
         self.assertEqual(allele.format.additional["sample_id"], "Sample001")
 

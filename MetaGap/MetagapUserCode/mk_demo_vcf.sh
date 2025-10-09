1f3b323a4f0c8c7deaa7fb60d5c6ea449d5bd84c
Stepanvar <84638149+Stepanvar@users.noreply.github.com>
2025-10-09 16:59:06 +0000

    ignore .venv

--- MetaGap/MetagapUserCode/mk_demo_vcf.sh
+++ MetaGap/MetagapUserCode/mk_demo_vcf.sh
@@ -8,20 +8,24 @@ mk() {
   printf '#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\t%s\n' "$sample" >> "$out"
 }
 
+sample_name() {
+  printf 'SAMPLE_%04d' "$1"
+}
+
 # A
-mk demo_vcfs/1.vcf SAMPLE_A
+mk demo_vcfs/1.vcf "$(sample_name 1)"
 printf $'20\t14370\trs6054257\tG\tA\t60\tPASS\tNS=1;DP=10\tGT:DP:GQ\t0/1:10:50\n20\t17330\t.\tT\tA\t40\tPASS\tNS=1;DP=12\tGT:DP:GQ\t0/0:12:60\n20\t1110696\trs6040355\tA\tG\t50\tPASS\tNS=1;DP=8\tGT:DP:GQ\t1/1:8:40\n' >> demo_vcfs/1.vcf
 
 # B
-mk demo_vcfs/2.vcf SAMPLE_B
+mk demo_vcfs/2.vcf "$(sample_name 2)"
 printf $'20\t14370\trs6054257\tG\tA\t44\tPASS\tNS=1;DP=9\tGT:DP:GQ\t0/0:9:50\n20\t17330\t.\tT\tA\t35\tPASS\tNS=1;DP=11\tGT:DP:GQ\t0/1:11:45\n20\t1110696\trs6040355\tA\tG\t55\tPASS\tNS=1;DP=7\tGT:DP:GQ\t0/1:7:48\n' >> demo_vcfs/2.vcf
 
 # C
-mk demo_vcfs/3.vcf SAMPLE_C
+mk demo_vcfs/3.vcf "$(sample_name 3)"
 printf $'20\t14370\trs6054257\tG\tA\t70\tPASS\tNS=1;DP=12\tGT:DP:GQ\t1/1:12:60\n20\t17330\t.\tT\tA\t42\tPASS\tNS=1;DP=10\tGT:DP:GQ\t0/0:10:55\n20\t1110696\trs6040355\tA\tG\t52\tPASS\tNS=1;DP=9\tGT:DP:GQ\t0/1:9:52\n' >> demo_vcfs/3.vcf
 
 # D
-mk demo_vcfs/4.vcf SAMPLE_D
+mk demo_vcfs/4.vcf "$(sample_name 4)"
 printf $'20\t14370\trs6054257\tG\tA\t50\tPASS\tNS=1;DP=0\tGT:DP:GQ\t./.:.:.\n20\t17330\t.\tT\tA\t60\tPASS\tNS=1;DP=13\tGT:DP:GQ\t1/1:13:62\n20\t1110696\trs6040355\tA\tG\t45\tPASS\tNS=1;DP=8\tGT:DP:GQ\t0/0:8:45\n' >> demo_vcfs/4.vcf
 
 # compress + index
